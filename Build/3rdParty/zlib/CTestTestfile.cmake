74510fc5b21c1c56d0b8a6d62984fc65d2d6f521
hgy413 <80718901@qq.com>
2017-01-13 10:03:04 +0800

    [+]remove build folder

--- Build/3rdParty/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/OpenSource/REDM/3rdParty/zlib
-# Build directory: F:/OpenSource/REDM/Build/3rdParty/zlib
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
