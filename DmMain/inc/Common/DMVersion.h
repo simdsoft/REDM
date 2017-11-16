cb13839578dae71862d43e6526b6fcf0b817c751
tcb <349685657@qq.com>
2017-11-16 13:50:38 +0800

    !11 为DM增加版本号及DMVersion更新工具 Merge pull request !11 from tcb/master

---
+++ DmMain/inc/Common/DMVersion.h
@@ -0,0 +1,30 @@
+// ----------------------------------------------------------------
+// Copyright (c)  
+// All rights reserved.
+// 
+// File name:	DMVersion.h
+// File mark:   
+// File summary:定义版本号
+// Author:		guoyouhuang
+// Edition:     1.0
+// Create date: 2017-3-16
+// ----------------------------------------------------------------
+#pragma once
+#define VER_MAJOR		1
+#define VER_MINOR		0
+#define VER_SLAVE		0
+#define VER_BUILD		165
+
+#define PRODUCTNAME     "DMMain"
+
+// 以下定义同时被RC和CPP文件使用，请不要修改
+#define VER			    VER_MAJOR.VER_MINOR.VER_SLAVE.VER_BUILD
+#define VER_KEY		    VER_MAJOR,VER_MINOR,VER_SLAVE,VER_BUILD
+
+#define __STR(x)		 #x
+#define _STR(x) 		__STR(x)
+#define __WSTR(x)		L##x
+#define _WSTR(x) 		__WSTR(x)
+
+#define VER_STR			_STR(VER)
+#define VER_WSTR		_WSTR(VER_STR)
