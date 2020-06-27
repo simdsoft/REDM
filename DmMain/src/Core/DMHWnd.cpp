160f07f851c5cef2a0579fea81cf9274bf1215ab
halx99 <halx99@live.com>
2020-06-27 13:21:58 +0800

    Simplfy code

--- DmMain/src/Core/DMHWnd.cpp
+++ DmMain/src/Core/DMHWnd.cpp
@@ -1392,7 +1392,7 @@ namespace DM
 				HRESULT hr = thisApp->m_fun_DwmSetWindowAttribute(m_hWnd, DWMWA_NCRENDERING_POLICY, &v, sizeof(v));
 
 				if (bEnabled && SUCCEEDED(hr)) {
-					MARGINS margins = { margin, margin, margin, margin };
+					MARGINS margins = { margin };
 					hr = thisApp->m_fun_DwmExtendFrameIntoClientArea(m_hWnd, &margins);
 					return SUCCEEDED(hr);
 				}
